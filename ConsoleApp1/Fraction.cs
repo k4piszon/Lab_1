6e41beeff0082d7a029324985450f6808a5f3b34
Kacper.Kotyza <Kacper.Kotyza@PK5-19>
2022-03-05 12:51:37 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Fraction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_1
+{
+    public class Fraction   //Utwórz klase ułamek
+    {
+        private double numerator { get; set; }         //Prywatna zmienna licznik
+        private double denominator { get; set; }       //Prywatna zmienna mianownik
+        
+        public Fraction()                              // Konstruktor domyślny
+        {
+            numerator = 1;
+            denominator = 2;
+        }
+
+        
+        public Fraction(double x, double y)             // Konstruktor zwykły
+        {
+            numerator = x;
+            denominator = y;
+        }
+        
+        public Fraction(Fraction previousFraction)      //Konstruktor kopiujący
+        {
+            numerator = previousFraction.numerator;
+            denominator = previousFraction.denominator;
+        }
+
+        public override string ToString()               //Dodaj metode ToString
+        {
+            return $"Numerator: {numerator}, denominator: {denominator}";
+        }
+
+        public static Fraction operator +(Fraction f1) => f1;
+        public static Fraction operator -(Fraction f1) => new Fraction(-f1.numerator, f1.denominator);
+        public static Fraction operator +(Fraction f1, Fraction f2) => new Fraction(f1.numerator * f2.denominator + f2.numerator * f1.denominator, f1.denominator * f2.denominator);
+        public static Fraction operator -(Fraction f1, Fraction f2) => new Fraction(f1.numerator * f2.denominator - f2.numerator * f1.denominator, f1.denominator * f2.denominator);
+
+    }
+}
+
+
+   
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction();
+            Fraction f2 = new Fraction(1, 3);
+            Console.WriteLine(f1); Console.WriteLine(f2); Console.WriteLine();
+            Console.WriteLine(-f1);
+            Console.WriteLine(f1 + f2);
+
+        }
+        string Konstruktory()
+        {
+            //Trzeba przekopiować do main
+            Fraction f1 = new Fraction();
+            Console.WriteLine(f1);
+
+            Fraction f2 = new Fraction(5, 7);
+            Console.WriteLine(f2);
+
+            Fraction f3 = new Fraction(f2);
+            Console.WriteLine(f3);
+            return "";
+        }
+    }
+}
+
